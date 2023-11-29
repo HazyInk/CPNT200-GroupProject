6d36fcbaacde31b09d78a4813188baa99378eff8
HazyInk <reillysheapaston@gmail.com>
2023-11-29 11:26:34 -0700

    added some placeholder design; navbar, footer, hero

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,69 @@
+<script>
+  const footerItems = [
+  {name: 'Github', href: "/"},
+  {name: 'Gitprojects', href: "/"},
+  {name: 'Googledocs', href: "/"}
+];
+</script>
+<footer>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d80236.93230320029!2d-114.24097500273436!3d51.06408939999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x53716f9268a69e81%3A0xb1db5e68bd61e28!2sSouthern%20Alberta%20Institute%20of%20Technology!5e0!3m2!1sen!2sca!4v1701280759048!5m2!1sen!2sca" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+<ul>
+  {#each footerItems as {name, href}}
+  <li><a {href}>{name}</a></li>
+  {/each}
+</ul>
+<p>&copy;Ashkie, Reilly & Emily</p>
+</footer>
+
+<style>
+iframe {
+  padding: 2rem;
+  border: #000000;
+  border-radius: 20px;
+  height: 200px;
+  width: 200px;
+}
+footer {
+  background-color: #ff9814;
+  border-radius: 20px;
+  width: 100%;
+  color: #ffff;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  font-family: 'Libre Baskerville', serif;
+}
+svg {
+  stroke:#ffff;
+  fill: #ffff;
+}
+
+svg:hover {
+  fill: #ffff;
+  stroke: #ffff;
+}
+ul{
+  display: flex;
+  justify-self: flex-end;
+  list-style-type: none;
+
+}
+
+  li {
+    padding: 1rem;
+    
+  }
+
+a {
+  list-style-type: none;
+  text-decoration: none;
+  color: #ffff;
+  padding: 0.4rem;
+  border-radius: 20%;
+}
+
+a:hover{
+  background: #ffff;
+  background: #c97100;
+}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
+<img id = "hero" src="personal.png" alt="alt">
+
+<div class="maintext">
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
 
 
 <script>
@@ -7,7 +11,20 @@
   let { Blog_Post } = data;
 </script>
 
+<div class="maintext">
 {#each data.Blog_Post as Blog_Post}
     <h1>{Blog_Post.Author}
     </h1>
   {/each}
+</div>
+
+<style>
+  .maintext {
+    text-align: center;
+  }
+  img {
+    border-radius: 1rem;
+    height: 500px;
+    width: 1450px;
+  }
+</style>
